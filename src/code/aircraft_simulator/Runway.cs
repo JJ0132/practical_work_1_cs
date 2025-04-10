fb4537d58fdee1fca96d4a4a1150ba1fb94a1d7d
javi12882 <javapro35@gmail.com>
2025-04-10 10:26:01 +0200

    Encode

---
+++ src/code/aircraft_simulator/Runway.cs
@@ -0,0 +1,57 @@
+public enum RunwayStatus
+{
+    Free,
+    Occupied
+}
+
+public class Runway
+{
+    private string id;
+    private RunwayStatus Status;
+
+    private Aircraft? CurrentAircraft;
+    private int TicksAvailability;
+    private int currentTicks;
+
+    public Runway(string id)
+    {
+        this.id = id;
+        TicksAvailability = 3;
+        Status = RunwayStatus.Free;
+        this.CurrentAircraft = null;
+    }
+
+    public RunwayStatus getStatus()
+    {
+        return this.Status;
+    }
+
+    public void RequestRunway(Aircraft s)
+    {
+        if(Status == RunwayStatus.Free)
+        {
+            Status = RunwayStatus.Occupied;
+            CurrentAircraft = s;
+
+        }
+    }
+    public void ReleaseRunway()
+    {
+        if (Status == RunwayStatus.Occupied)
+        {
+            Status = RunwayStatus.Free;
+            CurrentAircraft = null;
+            this.TicksAvailability = 3;
+        }
+    }
+
+    public int updateRunway()
+    {
+        if (this.TicksAvailability > 0)
+        {
+            this.TicksAvailability--;
+        }
+
+        return this.TicksAvailability;
+    }
+}
